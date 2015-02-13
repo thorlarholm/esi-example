52ad0b95ae038d06c390bd08f9eb26c84d5a584f
Thor Larholm <thor@larholm.com>
2015-02-13 13:30:03 +0100

    Moved HTML into views

--- server.js
+++ server.js
@@ -1,29 +1,32 @@
 var express = require('express');
 var esi = require('esi');
+var cons = require('consolidate');
 
-// Main application
 var app = express();
-app.set('port', process.env.PORT || 3000);
-app.listen(app.get('port'));
 
 // Setup ESI hooks
 app.use(esi);
 
 // Render a simple page with an ESI include
-function index(res, res, next) {
-	var snippetURL = 'http://localhost:' + app.get('port') + '/snippet';
-
-	var html = '<!DOCTYPE html><html><body><p>Index</p><esi:include src="' + snippetURL + '"></esi:include></body></html>'
-
-	res.send(html);
-
-}
-app.get('/', index);
+app.get('/', function index(req, res, next) {
+	console.log("Called index route");
+	res.render('index');
+});
 
 // Define our ESI include, could be external
-function snippet(res, res, next) {
-	res.send('<p>Hello from snippet included via ESI</p>');
-}
-app.get('/snippet', snippet);
+app.get('/snippet', function snippet(req, res, next) {
+	console.log("Called snippet route");
+	res.render('snippet');
+});
+
+// Port
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'));
+console.log('Listening on ' + app.get('port'));
+
+// Views
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 module.exports = app;
